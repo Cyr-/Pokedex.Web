a5b23224607cba20d6365180474106ec3d6f7c78
TJ <tjmatychuk@hotmail.com>
2016-09-19 16:39:05 -0500

    Initial commit

---
+++ Controllers/PokemonController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Pokedex.Pokeapi.Pokemon;
+using Pokedex.Logic.Web;
+
+namespace Pokedex.Web.Controllers
+{
+    public class PokemonController : Controller
+    {
+        public IActionResult Index(int id = 1)
+        {
+            var model = DataFetcher.GetApiObject<Pokemon>(new Uri("http://pokeapi.co/api/v2/pokemon/" + id)).Result;
+            return View(model);
+        }
+    }
+}
---
+++ Logic/DataFetcher.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Pokedex.Pokeapi.Common;
+
+namespace Pokedex.Logic.Web
+{
+    public class DataFetcher
+    {
+        private const string _uri = "http://pokeapi.co/api/v2/";
+
+        internal static HttpClient client = new HttpClient
+        {
+            BaseAddress = new Uri(_uri)
+        };
+
+        public static async Task<T> GetApiObject<T>(Uri url) where T : ApiObject => JsonConvert.DeserializeObject<T>(await client.GetStringAsync(url.AbsoluteUri));
+    }
+}
